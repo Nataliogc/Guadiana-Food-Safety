a4b177943d5e37666e39574af14d76272146612b
nataliohoteles-crypto <comunicaciones@hotelguadiana.es>
2026-05-20 04:08:02 +0200

    Configurar despliegue automatico en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,11 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Guadiana-Food-Safety/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
 });
+
